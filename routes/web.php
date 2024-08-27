25167dc72a9e456e5af78bf28e3c0c5d24add634
SoftSisPro <softsispro@gmail.com>
2024-08-26 22:27:57 -0500

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,20 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    #Return view('welcome');
+    return "Hello World";
 });
+
+Route::get('/posts', function () {
+    return "List of posts";
+});
+
+Route::get('/posts/create', function () {
+    return "form to create a post";
+});
+
+Route::get('/posts/{post}', function ($post) {
+    return "post with id: " . $post;
+});
+
+
