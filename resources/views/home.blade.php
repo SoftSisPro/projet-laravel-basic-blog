b7a736b2bdb8b671bf63c9c24050b7f6def0b237
SoftSisPro <softsispro@gmail.com>
2024-08-28 09:49:35 -0500

    Componentes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Laravel 11</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <h1>Bienvenido a mi pagina principal</h1>
+    <div class="container mx-auto bg-slate-700 p-5 h-screen text-white">
+        <h1 class='text-3xl'>Mi pagina de inicio</h1>
+        {{-- Alerta component anonimo --}}
+        <x-alert type="danger" class="mb-5">
+            <x-slot name="title">Titulo de alerta</x-slot>
+            Mi mensaje de alerta
+        </x-alert>
+        {{-- Alerta Componente de class --}}
+        <x-alert2 type="warning" class="mb-5">
+            <x-slot name="title">Titulo de alerta 2</x-slot>
+            Mi mensaje de alerta
+        </x-alert2>
+        <p>Hola mundo</p>
+    </div>
 </body>
 </html>
