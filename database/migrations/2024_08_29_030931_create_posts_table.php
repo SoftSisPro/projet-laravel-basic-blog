7d97dd041fc6b6c8e9052ea4478fefd938f46fc7
SoftSisPro <softsispro@gmail.com>
2024-08-29 21:36:35 -0500

    Eloquent

--- database/migrations/2024_08_29_030931_create_posts_table.php
+++ database/migrations/2024_08_29_030931_create_posts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->longtext('content');
+            $table->string('categoria');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 // - Rutas con controladores
@@ -17,3 +18,43 @@ Route::get('/', function () {
     return "Hello World";
 });
 */
+
+//- Creamos un ruta para cargar un modelo
+Route::get('prueba', function (){
+    // return "hola desde la ruta prueba"; Mostrar mensaje
+
+
+    /* //- Crear un nuevo post desde el modelo en la ruta
+    $post = new Post();
+
+    $post->title = 'Mi tercer post ';
+    $post->content = 'Contenido de mi tercer post';
+    $post->categoria = 'categoria 3';
+
+    $post->save();
+
+    return $post; */
+
+    /* //$post  = Post::find(2); // Buscar un post por id
+
+    $post = Post::where('title', 'Mi primer post')->first(); // Buscar un post por titulo
+    //- Actualizar un post en categoria
+    $post->categoria = "Desarrollo web";
+    $post->save();
+
+    return $post; */
+
+    //$post = Post::all(); // Mostrar todos los post
+
+    /* //- Consultar compleja de post
+    $post = Post::orderBy('categoria','asc') # Ordenar por categoria
+                ->select('id','title','categoria') # Campos a mostrar
+                ->take(2) # Limitar la cantidad de registros
+                ->get(); //- Listar
+
+    return $post; */
+
+    /* //- Eliminar un post
+    $post = Post::find(2);
+    $post->delete(); */
+});
