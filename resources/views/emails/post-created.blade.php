6143cd2a8163706dfef4f5659982df9dd2b8987a
SoftSisPro <softsispro@gmail.com>
2024-09-03 22:31:33 -0500

    markdown

--- resources/views/emails/post-created.blade.php
+++ resources/views/emails/post-created.blade.php
@@ -1,22 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Post Creado</h1>
-    <p>Se ha creado un nuevo post</p>
+<x-mail::message>
 
-    <br><br>
-    <h2>{{$post->title}}</h2>
-    <p>{{$post->content}}</p>
-    <p>{{$post->category}}</p>
-    <p>{{$post->slug}}</p>
-    <br>
-    <a href="{{ route('posts.show', $post) }}">Ir al Posts</a>
+## Se ha creado un nuevo post
 
-</body>
-</html>
+<x-mail::panel>
+<h2>{{$post->title}}</h2>
+<p>{{$post->content}}</p>
+<p>{{$post->category}}</p>
+<p>{{$post->slug}}</p>
+</x-mail::panel>
+
+<x-mail::button :url="route('posts.show', $post)" color="success">
+    Ir al Posts
+</x-mail::button>
+
+</x-mail::message>
